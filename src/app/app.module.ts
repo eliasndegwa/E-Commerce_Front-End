e17d3c2dc3e7879359a325aa71c1afdfd6882ab2
eliasndegwa <eliasndegwamwai@gmail.com>
2023-05-30 14:15:52 +0300

    add reset password popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
@@ -9,6 +10,7 @@ import { TagsComponent } from './tags/tags.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
     declarations: [
@@ -25,6 +27,7 @@ import { RegisterComponent } from './register/register.component';
     imports: [
         BrowserModule,
         FormsModule,
+        MatDialogModule,
         AppRoutingModule
     ]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component,OnInit,ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 // import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
@@ -10,14 +11,33 @@ import { NgForm } from '@angular/forms';
 // slideshow idea, void and wildcard use animation class as a basis
 //   ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   @ViewChild('form')form!:NgForm
 
+  constructor(private dialog:MatDialog){}
+  ngOnInit(): void {
+
+  }
   onLoginSubmit(){
 
   } 
 
   toRegisterPage(){
 
+  }
+  resetPassword(){
+
+  }
+  openModal(){
+    const modal= document.querySelector(".modal") as HTMLElement
+    if(modal!==null){
+      modal.style.display='block'
+    }
+  }
+  closeModal(){
+    const modal= document.querySelector(".modal") as HTMLElement
+    if(modal!==null){
+      modal.style.display='none'
+    }
   }
 }
